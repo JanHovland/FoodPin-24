5a9af474bd3b9f5da9d2c6a28690fa1ffbb62331
Jan Hovland <jan.hovland@lyse.net>
2018-10-17 19:13:51 +0200

    Kan nå hente fra iCloud via CloudKit

--- FoodPin/Controller/AboutTableViewController.swift
+++ FoodPin/Controller/AboutTableViewController.swift
@@ -24,13 +24,13 @@ class AboutTableViewController: UITableViewController {
         tableView.cellLayoutMarginsFollowReadableWidth = true
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        // Configure navigation bar appearance
+        // Configure navigation bar appearence
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
             navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
         }
-        
+
         tableView.tableFooterView = UIView()
     }
 
